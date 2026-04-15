d532b2f19703e49c9ca2e25a3882705780965524
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-04-15 09:03:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-const para = document.querySelector("p")
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const text = para.innerText
+const p = document.querySelector("p")
+const text = p.innerText
 
-para.addEventListener("mouseenter",() => {
-    // console.log("Entered");
-    
-    setInterval(()=>{
+const characters =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-        const str = text.split('').map((char,index)=> { 
-            return characters.split("")[Math.floor(Math.random()*53) ]
-        }).join("")
-        
-        para.innerText=str
+let iteration = 0
 
 
-    },30)
-})
+function randomText() {
+    const str = text.split("").map((char,index) => {
+        if(index < iteration){
+            return char 
+        }
+        return characters.split("")[Math.floor(Math.random() * 52) ]
+    }).join("")
 
+    p.innerText = str
+    iteration += 0.1 
+}
 
-// notes:
-// 1. .split("") : string ko array of characters me convert kardeta h , array of characters:- "aman" = ['a','m','a','n'];
-//2..split(" ") : Conversion on basisi of space into array elements. "aman yadav don": ['aman' , 'yadav' , 'don']
-//3. joins the elements of array to string : arr.join("") or arr.join(" "): join on basis of space.
- 
-
+setInterval(randomText,30)
